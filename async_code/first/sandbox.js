5babc9691f06c4f08c17c227c359e9241616141c
Pritam @ virtualbox <you@example.com>
2022-05-18 20:53:53 +0530

    nested callback

--- async_code/first/sandbox.js
+++ async_code/first/sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange',() => {
@@ -15,12 +15,17 @@ const getTodos = (callback) => {
     request.send();
 };
 
-// use callback function
-getTodos((err, data)=> {
-    console.log('callback fired');
-    if(err){
-        console.log(err);
-    }
-    else
+// nested callback function
+getTodos( 'todos/luigi.json' ,(err, data)=> {
+    console.log(data);
+
+    //
+    getTodos( 'todos/mario.json' ,(err, data)=> {
         console.log(data);
+        //
+        getTodos( 'todos/shaun.json' ,(err, data)=> {
+            console.log(data);
+        });
+    });
+
 });
