43e5cc5a43edccfedb76cfdfa8095cf71e88de33
Pritam @ virtualbox <you@example.com>
2022-05-18 20:45:27 +0530

    use json data

--- async_code/first/sandbox.js
+++ async_code/first/sandbox.js
@@ -2,18 +2,16 @@ const getTodos = (callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange',() => {
-        
         if(request.readyState === 4 && request.status === 200){
-            
-            callback(undefined, request.responseText);
-            
-        } else if (request.readyState === 4){
-            
+            const data= JSON.parse(request.responseText)
+            callback(undefined, data);
+        } 
+        else if (request.readyState === 4){            
             callback('could not fetch data', undefined);
         }
     });
-    
-    request.open("GET", 'https://jsonplaceholder.typicode.com/todos/');
+
+    request.open("GET", 'todos.json');
     request.send();
 };
 
