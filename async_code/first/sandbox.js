36910c94009652ac03fb5a3a5e6116b9b1650f74
Pritam @ virtualbox <you@example.com>
2022-05-18 21:13:59 +0530

    api using promsie

--- async_code/first/sandbox.js
+++ async_code/first/sandbox.js
@@ -1,33 +1,41 @@
 const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange',() => {
-        if(request.readyState === 4 && request.status === 200){
-            const data= JSON.parse(request.responseText)
-            callback(undefined, data);
-        } 
-        else if (request.readyState === 4){            
-            callback('could not fetch data', undefined);
-        }
+    return new Promise( (resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange',() => {
+            if(request.readyState === 4 && request.status === 200){
+                const data= JSON.parse(request.responseText)
+                resolve(data);
+            } 
+            else if (request.readyState === 4){            
+                reject('error getting resource ');
+            }
+        });
+        request.open("GET", 'todos.json');
+        request.send();    
     });
-
-    request.open("GET", 'todos.json');
-    request.send();
 };
+
+getTodos('todos/luiaklsdjgis.json').then( data => {
+    console.log('promise resolved: ', data);
+}).catch( err => {
+    console.log('promise rejected: ', err);
+});
 
 
 // promise example
 
-const getSomething = () => {
+// const getSomething = () => {
 
-    return new Promise((resolve, reject) => {
-        // fetch something
-        // if success
-        resolve('some data');
-        // if there is an error
-        reject('some error')
-    });
-};
+//     return new Promise((resolve, reject) => {
+//         // fetch something
+//         // if success
+//         resolve('some data');
+//         // if there is an error
+//         reject('some error')
+//     });
+// };
 
 // return promise
 // getSomething().then((data) => {
@@ -39,8 +47,8 @@ const getSomething = () => {
 // );
 
 
-getSomething().then(data =>{
-    console.log(data);
-}).catch(err => {
-    console.log(err);
-});
+// getSomething().then(data =>{
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
