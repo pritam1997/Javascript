141ef318f66e0978fdab742f5f9c88853e981b04
Pritam @ virtualbox <you@example.com>
2022-05-18 20:36:23 +0530

    use callback function to fetch api with error and response data

--- async_code/first/sandbox.js
+++ async_code/first/sandbox.js
@@ -1,18 +1,28 @@
-
+const getTodos = (callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange',() => {
-        // console.log(request, request.readyState);
+        
         if(request.readyState === 4 && request.status === 200){
-            console.log(request, request.responseText);
+            
+            callback(undefined, request.responseText);
             
         } else if (request.readyState === 4){
-            console.log('coudl not fetch data');
             
+            callback('could not fetch data', undefined);
         }
     });
-
+    
     request.open("GET", 'https://jsonplaceholder.typicode.com/todos/');
     request.send();
+};
 
-
+// use callback function
+getTodos((err, data)=> {
+    console.log('callback fired');
+    if(err){
+        console.log(err);
+    }
+    else
+        console.log(data);
+});
